84962280cda930d215a2d2cb511d4f65ccccf3a0
Aneli64 <eliandeniz27@gmail.com>
2023-11-24 20:35:32 +0100

    Initial commit

---
+++ app/src/main/java/com/example/superheroes/ui/theme/SuperHero.kt
@@ -0,0 +1,80 @@
+package com.example.superheroes.ui.theme
+
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.R
+import com.example.superheroes.Superhero
+
+fun getSuperheroes(): List<Superhero> {
+    return listOf(
+        Superhero("Spiderman", "Petter Parker", "Marvel", R.drawable.spiderman),
+        Superhero("Wolverine", "James Howlett", "Marvel", R.drawable.logan),
+        Superhero("Batman", "Bruce Wayne", "DC", R.drawable.batman),
+        Superhero("Thor", "Thor Odinson", "Marvel", R.drawable.thor),
+        Superhero("Flash", "Jay Garrick", "DC", R.drawable.flash),
+        Superhero("Green Lantern", "Alan Scott", "DC", R.drawable.green_lantern),
+        Superhero("Wonder Woman", "Princess Diana", "DC", R.drawable.wonder_woman)
+    )
+}
+
+@Preview
+@Composable
+fun SuperHeroView() {
+    val context = LocalContext.current
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(130.dp),
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(horizontal = 4.dp, vertical = 4.dp)
+    ) {
+        items(getSuperheroes()) { superhero ->
+            ItemHero(superhero = superhero, onItemSelected = { Toast.makeText(context, it.superheroName, Toast.LENGTH_SHORT).show()})
+        }
+    }
+}
+
+@Composable
+fun ItemHero(superhero: Superhero, onItemSelected: (Superhero) ->
+Unit) {
+    Card(
+        border = BorderStroke(2.dp, Color.Red),
+        modifier = Modifier
+            .width(200.dp)
+            .clickable { onItemSelected(superhero) }
+    ) {
+        Image(
+            painter = painterResource(id = superhero.photo),
+            contentDescription = superhero.superheroName,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Text(text = superhero.superheroName, Modifier.align(Alignment.CenterHorizontally))
+        Text(text = superhero.realName, Modifier.align(Alignment.CenterHorizontally))
+    }
+}
